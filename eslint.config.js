05e6c706b18d8732868620cc9ebba53403df9713
Siddhanta Panda <43051773+spanda38@users.noreply.github.com>
2025-08-28 19:02:40 -0400

    Swag

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -5,13 +6,19 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    // Ignore build output and node_modules
+    ignores: ["dist", "node_modules"],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node, // so Node globals like __dirname don’t error
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -23,7 +30,13 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+
+      // TypeScript rules
       "@typescript-eslint/no-unused-vars": "off",
+
+      // Disable Next.js-specific rules (you don’t use Next.js)
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
     },
   }
 );
